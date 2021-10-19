8b0fd19c30864da97387484b04d535dce416a972
OlenaAwd <elena.avdiunina@gmail.com>
2021-10-20 00:31:41 +0300

    finish cart

---
+++ src/data.js
@@ -0,0 +1,28 @@
+import img from './img/fruits.png'
+
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Banana',
+      price: 10,
+      image: img,
+      installment: ''
+    },
+    {
+      id: '2',
+      name: 'Apple',
+      price: 8,
+      image: img,
+      installment: ''
+    },
+    {
+      id: '3',
+      name: 'Papaya',
+      price: 10,
+      image: img,
+      installment: 3
+    },
+  ],
+};
+export default data;
